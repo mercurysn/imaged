917bd621a6264d4c952bcc25a078dee653cc77ef
SimonN <simon.ng@au.pinpoint.biz>
2015-02-23 14:03:04 +1100

    Added code for ImageD

---
+++ ImageD.IntegrationTests/ImageDownloaderTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Net;
+using NUnit.Framework;
+
+namespace ImageD.IntegrationTests
+{
+    [TestFixture]
+    public class ImageDownloaderTests
+    {
+        [Test]
+        public void Test()
+        {
+            const string DOWNLOAD_PATH = @"C:\temp\";
+            string fileName = @"Test-" + Guid.NewGuid();
+            const string URL = @"https://www.atlassian.com//wac/software/jira/tourBlocks/0/screenshotTourSection/0/imageBinary/jiraagile-02_whyja_1_flexibleplanning.png";
+
+            WebProxy proxy = new WebProxy("aujcproxy", 80)
+                             {
+                                 Credentials = CredentialCache.DefaultCredentials
+                             };
+
+            WebClient webClient = new WebClient
+                            {
+                                Proxy = proxy
+                            };
+
+
+            ImageDownloader downloader = new ImageDownloader(webClient);
+
+            downloader.DownloadImage(DOWNLOAD_PATH, fileName, URL);
+        }
+    }
+}
---
+++ ImageD.UnitTests/ImageDownloaderTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace ImageD.UnitTests
+{
+    [TestFixture]
+    public class ImageDownloaderTests
+    {
+        [Test]
+        public void TestConstructPath()
+        {
+            const string DOWNLOAD_PATH = @"C:\temp\";
+            const string FILE_NAME = @"Test";
+            const string URL = @"https://www.atlassian.com//wac/software/jira/tourBlocks/0/screenshotTourSection/0/imageBinary/jiraagile-02_whyja_1_flexibleplanning.png";
+
+            ImageDownloader downloader = new ImageDownloader();
+
+            var downloadLocation = downloader.ConstructDownloadFilePath(DOWNLOAD_PATH, FILE_NAME, URL);
+
+            Assert.AreEqual(@"C:\temp\Test.png", downloadLocation);
+        }
+
+    }
+}
