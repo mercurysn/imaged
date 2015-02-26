c622d1fdce482f74750a9deaf090a2cd04037589
SimonN <simon.ng@au.pinpoint.biz>
2015-02-26 16:05:57 +1100

    Adding ImageDownloader back into the solution

---
+++ ImageD/ImageDownloader.cs
@@ -0,0 +1,37 @@
+﻿using System.IO;
+using System.Net;
+
+namespace ImageD
+{
+    public class ImageDownloader
+    {
+        private WebClient _webClient = new WebClient();
+
+        public ImageDownloader()
+        {
+            
+        }
+
+        public ImageDownloader(WebClient webClient)
+        {
+            _webClient = webClient;
+        }
+
+
+        public string DownloadImage(string downloadPath, string fileName, string url)
+        {
+            string downloadLocationPath = ConstructDownloadFilePath(downloadPath, fileName, url);
+            using (WebClient client = _webClient)
+            {
+                client.DownloadFile(url, downloadLocationPath);
+            }
+
+            return downloadLocationPath;
+        }
+
+        public string ConstructDownloadFilePath(string downloadPath, string fileName, string url)
+        {
+            return string.Concat(downloadPath, fileName, Path.GetExtension(url));
+        }
+    }
+}
